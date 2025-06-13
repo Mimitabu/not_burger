3d3b6644b79551d0823194b0e82118693a7a19a3
Marie S <morefly@ya.ru>
2025-06-13 16:28:06 +0300

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
 import Counter from "./components/Counter";
 import './index.scss';
+import { Route, Routes } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
+import { MainPageLazy } from "./pages/MainPage/MainPage.lazy";
+import { Suspense } from "react";
 
 const App = () => {
     return (
         <div className="app">
-            asdasdASFASFa
+            <Link to={'/'}>Main</Link>
+            <Link to={'/about'}>About</Link>
+            <Suspense fallback={<>Loading...</>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageLazy />} />
+                    <Route path={'/'} element={<MainPageLazy />} />
+                </Routes>
+            </Suspense>
             <Counter />
         </div>
     );
