cddce593f6fff61e8707b8eefceb384478c7417c
Marie S <morefly@ya.ru>
2025-06-13 17:20:12 +0300

    add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import Counter from "./components/Counter";
-import './index.scss';
-import { Route, Routes } from 'react-router-dom';
-import { Link } from "react-router-dom";
-import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
-import { MainPageLazy } from "./pages/MainPage/MainPage.lazy";
-import { Suspense } from "react";
+import React, {Suspense} from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
+import './styles/index.scss';
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
     return (
-        <div className="app">
-            <Link to={'/'}>Main</Link>
-            <Link to={'/about'}>About</Link>
-            <Suspense fallback={<>Loading...</>}>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageLazy />} />
-                    <Route path={'/'} element={<MainPageLazy />} />
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                    <Route path={'/'} element={<MainPageAsync />} />
                 </Routes>
             </Suspense>
-            <Counter />
         </div>
     );
 };
