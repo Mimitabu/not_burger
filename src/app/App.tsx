f8ae13cbefbd1a08d62134c52da98700de0b2770
Marie S <morefly@ya.ru>
2025-07-04 18:19:30 +0300

    add first UI Kit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import './styles/index.scss';
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import { AppRouter } from './providers/router';
+import { Navbar } from 'widgets/Navbar';
 
 
 const App = () => {
@@ -10,9 +11,8 @@ const App = () => {
 
     return (
         <div className={classNames('app', {}, [theme])}>
+            <Navbar />
             <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
             <AppRouter />
         </div>
     );
